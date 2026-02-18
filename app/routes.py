a62e16d432d870924e4e66343ba97385188b7578
karlmage <karlmagegit@gmail.com>
2026-02-18 16:10:14 +0100

    Add working Flask template

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import Flask, Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/home')
+@main.route('/')
+def index():
+    return render_template("index.html")
